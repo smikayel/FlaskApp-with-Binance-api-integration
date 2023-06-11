5b30c599b0282376f23ca7ceaef01b03929a1579
smikayel <sasha-mikayelyan@mail.ru>
2023-06-12 01:06:38 +0400

    fix template

--- apps/candlestick.py
+++ apps/candlestick.py
@@ -7,17 +7,20 @@ candlestick = Blueprint('candlestick', __name__)
 
 @candlestick.route('/', methods=["GET"])
 def candlestick_route():
-    candlestick_data = retrieve_candlestick_data()
-    candlestick_chart = go.Candlestick(
-        x=candlestick_data[0]['x'],
-        open=candlestick_data[0]['open'],
-        high=candlestick_data[0]['high'],
-        low=candlestick_data[0]['low'],
-        close=candlestick_data[0]['close'],
-        increasing_line_color='green',
-        decreasing_line_color='red'
-    )
-    candlestick_data = [candlestick_chart]
+    candlestick_data_arr = retrieve_candlestick_data()
+    candlestick_data = []
+    for data in candlestick_data_arr:
+        candlestick_chart = go.Candlestick(
+            x=data['x'],
+            open=data['open'],
+            high=data['high'],
+            low=data['low'],
+            close=data['close'],
+            increasing_line_color='green',
+            decreasing_line_color='red'
+        )
+
+        candlestick_data.append(candlestick_chart)
     symbols = ["BTC", "ETH", "XRP", "LTC", "BCH", "ADA", "DOT", "XLM", "LINK", "USDT"]
     market_caps = [1000000, 2000000, 1500000, 800000, 1200000, 900000, 700000, 600000, 500000, 1000000]
     pie_chart = go.Pie(
