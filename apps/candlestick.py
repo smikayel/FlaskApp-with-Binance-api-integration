d83ccdcc24235c32bfa79c2396111edb2816397d
smikayel <sasha-mikayelyan@mail.ru>
2023-06-12 00:46:34 +0400

    finlisation of the app

--- apps/candlestick.py
+++ apps/candlestick.py
@@ -1,13 +1,33 @@
 from flask import Blueprint, render_template
 from .helpers.retrieve_candlestick import retrieve_candlestick_data
 from .helpers.retrieve_pie_chart import retrieve_pie_chart_data
-
+import plotly.graph_objs as go
+import datetime
 candlestick = Blueprint('candlestick', __name__)
 
 
 @candlestick.route('/', methods=["GET"])
 def candlestick_route():
     candlestick_data = retrieve_candlestick_data()
-    pie_chart_data = retrieve_pie_chart_data()
+    candlestick_chart = go.Candlestick(
+        x=candlestick_data[0]['x'],
+        open=candlestick_data[0]['open'],
+        high=candlestick_data[0]['high'],
+        low=candlestick_data[0]['low'],
+        close=candlestick_data[0]['close'],
+        increasing_line_color='green',
+        decreasing_line_color='red'
+    )
+    candlestick_data = [candlestick_chart]
+    symbols = ["BTC", "ETH", "XRP", "LTC", "BCH", "ADA", "DOT", "XLM", "LINK", "USDT"]
+    market_caps = [1000000, 2000000, 1500000, 800000, 1200000, 900000, 700000, 600000, 500000, 1000000]
+    pie_chart = go.Pie(
+        labels=symbols,
+        values=market_caps
+    )
+    pie_data = [pie_chart]
 
-    return render_template('index.html', candlestick_data=candlestick_data, pie_chart_data=pie_chart_data)
+    candlestick_div = go.Figure(data=candlestick_data).to_html(full_html=False)
+    pie_div = go.Figure(data=pie_data).to_html(full_html=False)
+
+    return render_template('index.html', candlestick_div=candlestick_div, pie_div=pie_div)
--- apps/helpers/retrieve_candlestick.py
+++ apps/helpers/retrieve_candlestick.py
@@ -4,13 +4,14 @@ from ..models.tickerDataModel import TickerData
 def retrieve_candlestick_data():
     candlestick_data = TickerData.query.all()
     candlestick_chart_data = []
+
     for data in candlestick_data:
         candlestick = {
             "x": [data.open_time, data.close_time],
-            "open": data.open,
-            "high": data.high,
-            "low": data.low,
-            "close": data.close,
+            "open": [data.open],
+            "high": [data.high],
+            "low": [data.low],
+            "close": [data.close],
         }
         candlestick_chart_data.append(candlestick)
 
